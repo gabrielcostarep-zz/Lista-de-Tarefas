375ecadd7620cad92bbaafccd1833c80c9250483
Gabriel Costa <gabrielcosta12377@hotmail.com>
2021-08-02 00:08:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
+// Toda vez que a pagina for recarregada, essa funcao sera chamada
 onload = function () {
     if (tarefas == null) {
         let lista = document.getElementById("ul");
         lista.style.display = "none";
     } else {
+
+        // Funcao para registrar as tarefas
         function regist() {
             let lista = document.getElementById("ul");
             let conteudo = "";
@@ -22,6 +25,7 @@ onload = function () {
     }
 }
 
+// Funcao para adicionar uma nova tarefa
 function addTask() {
     let inp = document.getElementById("tarefa").value;
     let hr = document.getElementById("hours").value;
@@ -31,12 +35,15 @@ function addTask() {
     } else if (hr === "") {
         alert("Insira um horário")
     } else {
+
+        // Salvando os dados coletados
         tarefas.push(inp + " - " + hr);
         localStorage.setItem("tarefas", JSON.stringify(tarefas));
         location.reload();
     }
 }
 
+// Funcao para apagar todas as tarefas
 function apagarTudo() {
     localStorage.clear();
     location.reload();
